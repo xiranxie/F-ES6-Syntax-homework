f6f2959b8866b7c8a96f1c95b5abf780b4e2ba0e
Xiran Xie <xiran.xie@thoughtworks.com>
2021-03-15 20:30:07 +0800

    pass two practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,20 @@
-const parseData
+const parseUser = (keys,user) =>{
+    var parsedUser = {};
+    keys.map((v,i)=>{
+        parsedUser[keys[i]] = user[i];
+    })
+    return parsedUser;
+}
+
+const parseData = (obj) =>{
+    var keys = [];
+    var parsedData = [];
+    for(let col of obj.column){
+        keys.push(Object.values(col)[0]);
+    }
+    for(let user of obj.data){
+        parsedData.push(parseUser(keys,user));
+    }
+    return parsedData;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections) =>{
+    var reverse_sections = sections.reverse();
+    for(let section of sections){
+        items.splice(section.index,0,section.content);
+    }
+    return items;
+}
 export { inject };
